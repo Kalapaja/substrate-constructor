7791cfce487f9675273f2e9540ebaf53f304d178
Fluid <90795031+fluiderson@users.noreply.github.com>
2024-05-28 11:35:45 +0300

    feat: add `Error` implementations for error types

--- src/error.rs
+++ src/error.rs
@@ -1,28 +1,38 @@
 use external_memory_tools::ExternalMemory;
+use std::fmt::{Debug, Display, Formatter, Result};
 use substrate_parser::{
     error::{ExtensionsError, RegistryError},
     traits::AsMetadata,
 };
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum ErrorFixMe<E: ExternalMemory, M: AsMetadata<E>> {
     ExtensionsList(ExtensionsError),
     ExtraNotInExtensions,
     MetaStructure(M::MetaStructureError),
-    Registry(RegistryError<E>),
+    Registry(#[from] RegistryError<E>),
     UnexpectedVariantIndex,
     UnfinalizedExtension,
     WrongExtraStructure,
 }
 
-impl<E: ExternalMemory, M: AsMetadata<E>> From<RegistryError<E>> for ErrorFixMe<E, M> {
-    fn from(registry_error: RegistryError<E>) -> Self {
-        ErrorFixMe::Registry(registry_error)
+// TODO: provide actual error descriptions.
+impl<E: ExternalMemory, M: AsMetadata<E>> Display for ErrorFixMe<E, M> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        <Self as Debug>::fmt(self, f)
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum StorageRegistryError {
     MapHashesNotATuple,
     MapHashesNumberMismatch,
+}
+
+// TODO: provide actual error descriptions.
+impl Display for StorageRegistryError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        <Self as Debug>::fmt(self, f)
+    }
 }
